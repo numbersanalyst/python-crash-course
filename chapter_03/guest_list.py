11d8a0d717bf3a689117949b9447b619cabb9df7
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-08-29 23:51:49 +0200

    Add exercise 3.4

---
+++ chapter_03/guest_list.py
@@ -0,0 +1,7 @@
+guests = ['Kamil', 'Marian', 'Konrad']
+
+message = 'Jesteś zaproszony na obiad,'
+
+print(f'{message} {guests[0]}!')
+print(f'{message} {guests[1]}!')
+print(f'{message} {guests[2]}!')
