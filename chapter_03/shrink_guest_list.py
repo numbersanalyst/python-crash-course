f7a964897468f413a78ebc82246e2abe3ee80eec
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-09-01 23:58:43 +0200

    Add exercise 3.7

---
+++ chapter_03/shrink_guest_list.py
@@ -0,0 +1,35 @@
+# 3.7
+
+guests = ['Kamil', 'Marian', 'Konrad']
+
+message = 'Jesteś zaproszony na obiad,'
+
+print(f'{message} {guests[0]}!')
+print(f'{message} {guests[1]}!')
+print(f'{message} {guests[2]}!')
+
+print(f'{guests[2]} nie może przyjść.')
+
+guests[2] = 'Marek'
+
+print(f'{message} {guests[0]}!')
+print(f'{message} {guests[1]}!')
+print(f'{message} {guests[2]}!')
+
+print('Znalazło się więcej miejsc, będzie nowy stół.')
+
+guests.insert(0,'Waldek')
+guests.insert(2, 'Ludwik')
+guests.append('Ryszard')
+
+print(f'{message} {guests[0]}!')
+print(f'{message} {guests[1]}!')
+print(f'{message} {guests[2]}!')
+print(f'{message} {guests[3]}!')
+print(f'{message} {guests[4]}!')
+print(f'{message} {guests[5]}!')
+
+print('Jednak można zaprosić tylko dwie osoby...')
+
+guests.pop()
+guests.pop()
