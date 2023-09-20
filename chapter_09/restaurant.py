7a615ea0b272775748f287abbdc0071119c4703d
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-09-20 21:33:16 +0200

    Add exercise 9.1

---
+++ chapter_09/restaurant.py
@@ -0,0 +1,23 @@
+class Restaurant():
+    """A class representing a restaurant."""
+    def __init__(self, restaurant_name, cuisine_type):
+        """Restaurant constructor."""
+        self.name = restaurant_name
+        self.cuisine = cuisine_type
+
+    def describe_restaurant(self):
+        """Display the information about a restaurant."""
+        print(f'Restaurant name: {self.name}')
+        print(f'Cuisine type: {self.cuisine}')
+
+    def open_restaurant(self):
+        """Display a message that the restaurant is open."""
+        print(f'Restaurant {self.name} is open, come on!')
+
+restaurant = Restaurant('American Burgers&Steaks', 'Fast food')
+
+print(restaurant.name)
+print(restaurant.cuisine)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
