f23d240bedc64647880fd235479b3b8490007926
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-09-28 21:47:57 +0200

    Add exercise 9.7 and 9.8

---
+++ chapter_09/admin.py
@@ -0,0 +1,51 @@
+class User():
+    """A class representing an user."""
+
+    def __init__(self, first_name, last_name, nickname, age):
+        """User constructor."""
+        self.f_name = first_name.title()
+        self.l_name = last_name.title()
+        self.nick = nickname
+        self.age = age
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Display information about user."""
+        print(f'\n{self.f_name} {self.l_name}')
+        print(f'Nickname: {self.nick}')
+        print(f'Age: {self.age}')
+
+    def greet_user(self):
+        """Display a greeting message for the user."""
+        msg = f'Nice to see you {self.f_name} {self.l_name}!'
+        print(f'\n{msg}')
+
+    def inceremnt_login(self):
+        """Increment the login counter."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset the login attempt counter."""
+        self.login_attempts = 0
+
+
+class Admin(User):
+    """Class representing admin account."""
+
+    def __init__(self, f_name, l_name, nick, age):
+        """Admin account constructor."""
+        super().__init__(f_name, l_name, nick, age)
+        self.privilages = []
+
+    def show_privilages(self):
+        """Show the list of user privilages."""
+        print(f'\nList of that user privilages:')
+        for privilage in self.privilages:
+            print(f'- {privilage}')
+
+admin = Admin('John', 'Johnson', 'Johny', 26)
+admin.privilages = ['post', 'delete_post', 'ban', 'mute']
+
+
+admin.describe_user()
+admin.show_privilages()
