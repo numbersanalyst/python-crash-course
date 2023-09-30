a3ca81482938fc008f6f62163191b0a379c6a121
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-01 00:38:44 +0200

    Add exercise 9.13

---
+++ chapter_09/dice.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+class Die():
+    """A class representing an die throw."""
+
+    def __init__(self, num_of_sides=6):
+        """A die constructor."""
+        self.sides = num_of_sides
+        print(f'\n\tA die with {num_of_sides} sides has created.')
+
+    def roll(self):
+        """Simulate a die roll."""
+        print('\nRolling...')
+        number = randint(1,self.sides)
+        print(f'The drawn number is {number}.')
+
+die1 = Die()
+for i in range(10):
+    die1.roll()
+
+die2 = Die(10)
+for i in range(10):
+    die2.roll()
+
+die3 = Die(20)
+for i in range(10):
+    die3.roll()
