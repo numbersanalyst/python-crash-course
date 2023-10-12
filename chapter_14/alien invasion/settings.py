13b3c333c87d3e250c90fb55e9c0b6d7d77dc143
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-12 20:15:32 +0200

    Create difficulty levels for the alien invasion (ex. 14.4).

--- chapter_14/alien invasion/settings.py
+++ chapter_14/alien invasion/settings.py
@@ -7,26 +7,40 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        # Ship settings
-        self.ship_limit = 3
         # Bullet settings
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
         # Aliens settings
         self.fleet_drop_speed = 10
 
         # Easy change speed game
         self.speedup_scale = 1.1
 
+        self.difficulty_level = 'medium'
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that will be change trough the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        if self.difficulty_level == 'easy':
+            self.ship_speed = 1.5
+            self.bullet_speed = 2.0
+            self.alien_speed = 0.8
+            self.bullets_allowed = 5
+            self.ship_limit = 8
+        elif self.difficulty_level == 'medium':
+            self.ship_speed = 1.5
+            self.bullet_speed = 3.0
+            self.alien_speed = 1.0
+            self.bullets_allowed = 3
+            self.ship_limit = 3
+        elif self.difficulty_level == 'difficult':
+            self.ship_speed = 2.5
+            self.bullet_speed = 2.5
+            self.alien_speed = 1.2
+            self.bullets_allowed = 2
+            self.ship_limit = 1
 
         # Value fleet_direction 1 means right, 0 means left
         self.fleet_direction = 1
