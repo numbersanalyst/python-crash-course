83359010d9ca70cb1c424b980573191e6c87fb36
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-15 20:30:54 +0200

    Update difficulty settings.

--- chapter_14/alien invasion/settings.py
+++ chapter_14/alien invasion/settings.py
@@ -26,26 +26,26 @@ class Settings:
     def initialize_dynamic_settings(self):
         """Initialize settings that will be change trough the game."""
         if self.difficulty_level == 'easy':
-            self.ship_speed = 1.5
-            self.bullet_speed = 2.0
-            self.alien_speed = 0.8
-            self.bullets_allowed = 5
-            self.ship_limit = 3
+            self.ship_limit = 5
+            self.bullets_allowed = 10
+            self.ship_speed = 0.75
+            self.bullet_speed = 1.5
+            self.alien_speed = 0.5
             self.alien_points = 50
         elif self.difficulty_level == 'medium':
+            self.ship_limit = 3
+            self.bullets_allowed = 3
             self.ship_speed = 1.5
             self.bullet_speed = 3.0
             self.alien_speed = 1.0
-            self.bullets_allowed = 3
-            self.ship_limit = 2
-            self.alien_points = 35
+            self.alien_points = 75
         elif self.difficulty_level == 'difficult':
-            self.ship_speed = 2.5
-            self.bullet_speed = 2.5
-            self.alien_speed = 1.2
-            self.bullets_allowed = 2
-            self.ship_limit = 1
-            self.alien_points = 10
+            self.ship_limit = 2
+            self.bullets_allowed = 3
+            self.ship_speed = 3.0
+            self.bullet_speed = 6.0
+            self.alien_speed = 2.0
+            self.alien_points = 150
 
         # Value fleet_direction 1 means right, 0 means left
         self.fleet_direction = 1
