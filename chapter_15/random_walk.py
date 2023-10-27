079e6a1c7ab255461a941d6fd25537fbd8ce4d6c
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-27 06:36:17 +0200

    Add exercise 15.5.

--- chapter_15/random_walk.py
+++ chapter_15/random_walk.py
@@ -13,13 +13,8 @@ class RandomWalk():
     def fill_walk(self):
         """Wygenerowanie wszystkich punktów dla błądzenia losowego."""
         while len(self.x_values) < self.num_points:
-            x_direction = choice([1,-1])
-            x_distance = choice([0,1,2,3,4])
-            x_step = x_direction*x_distance
-
-            y_direction = choice([1,-1])
-            y_distance = choice([0,1,2,3,4])
-            y_step = y_direction*y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             if x_step == 0 and y_step == 0:
                 continue
@@ -29,3 +24,9 @@ class RandomWalk():
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        """Generuje nowy losowy punkt."""
+        direction = choice([1,-1])
+        distance = choice([0,1,2,3,4])
+        return direction*distance
