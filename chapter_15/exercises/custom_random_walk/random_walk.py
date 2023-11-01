70c38ea10452a7074afd27364d589478ecdc7298
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-11-01 23:59:52 +0100

    Add exercise 15.4

---
+++ chapter_15/exercises/custom_random_walk/random_walk.py
@@ -0,0 +1,33 @@
+from random import choice
+
+
+class RandomWalk:
+    """Klasa przeznaczona do wygenerowania błądzenia losowego."""
+
+    def __init__(self, num_points=5000):
+        """Inicjalizacja atrybutów błądzenia."""
+        self.num_points = num_points
+
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """Wygenerowanie wszystkich punktów dla błądzenia losowego."""
+        while len(self.x_values) < self.num_points:
+            x_step = self.get_step()
+            y_step = self.get_step()
+
+            if x_step == 0 and y_step == 0:
+                continue
+
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+    def get_step(self):
+        """Generuje nowy losowy punkt."""
+        direction = choice([2, 1, -1, -2])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        return direction * distance
