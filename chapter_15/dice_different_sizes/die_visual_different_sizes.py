b216a57709560ca4d4a2f4a43fd4c8e84f5401fa
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-11-03 14:42:56 +0100

    Add some lesson from chapter 15

---
+++ chapter_15/dice_different_sizes/die_visual_different_sizes.py
@@ -0,0 +1,30 @@
+from die import Die
+from plotly import offline
+from plotly.graph_objs import Bar, Layout
+
+die1 = Die()
+die2 = Die(10)
+
+results = [die1.roll() + die2.roll() for _ in range(50_000)]
+
+max_result = die1.num_sides + die2.num_sides
+
+frequencies = [results.count(value) for value in range(2, max_result + 1)]
+
+x_values = list(range(2, max_result + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title": "Results", "dtick": 1}
+y_axis_config = {"title": "Frequency of occurance values"}
+my_layout = Layout(
+    title="Result of throwing two die D6 fiveteen hundred times.",
+    xaxis=x_axis_config,
+    yaxis=y_axis_config,
+)
+offline.plot(
+    {
+        "data": data,
+        "layout": my_layout,
+    },
+    filename="d6_d10.html",
+)
---
+++ chapter_15/dice_throw/die_visual.py
@@ -0,0 +1,28 @@
+from die import Die
+from plotly import offline
+from plotly.graph_objs import Bar, Layout
+
+die = Die()
+
+results = [die.roll() for _ in range(1_000)]
+
+frequencies = [results.count(value) for value in range(1, die.num_sides + 1)]
+
+x_values = list(range(1, die.num_sides + 1))
+
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title": "Results"}
+y_axis_config = {"title": "Frequency of occurance values"}
+my_layout = Layout(
+    title="Result of throwing single die D6 one hundred times.",
+    xaxis=x_axis_config,
+    yaxis=y_axis_config,
+)
+offline.plot(
+    {
+        "data": data,
+        "layout": my_layout,
+    },
+    filename="d6.html",
+)
