1389e450015bdc3956a7f3195877feabb06171aa
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-11-13 06:53:08 +0100

    Add new lessons.

--- chapter_16/mapping_global_data_sets/eq_plot.py
+++ chapter_16/mapping_global_data_sets/eq_plot.py
@@ -1,11 +1,13 @@
 import json
 
+from plotly import offline
+from plotly.graph_objs import Layout, Scattergeo
+
 filename = "data/eq_data_1_day_m1.json"
 with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data["features"]
-print(len(all_eq_dicts))
 
 mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
@@ -16,10 +18,9 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
 
-readable_file = "data/readable_eq_data.json"
-with open(readable_file, "w") as f:
-    json.dump(all_eq_data, f, indent=4)
+data = [{"type": "scattergeo", "lon": lons, "lat": lats}]
+my_layout = Layout(title="Earthquakes in the world")
+
+fig = {"data": data, "layout": my_layout}
+offline.plot(fig, filename="global_earthquakes.html")
