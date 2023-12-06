9dcbf3627e084e6b77507e616d94d2d9bd2e7ce5
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-06 23:41:20 +0100

    Add subpages to pizzas app (ex. 18.8).

--- chapter_18/exercises/pizza_project/pizzas/urls.py
+++ chapter_18/exercises/pizza_project/pizzas/urls.py
@@ -7,4 +7,6 @@ from . import views
 app_name = "pizzas"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("pizzas", views.pizzas, name="pizzas"),
+    path("pizza/<int:pizza_id>", views.pizza, name="pizza"),
 ]
--- chapter_18/exercises/pizza_project/pizzas/views.py
+++ chapter_18/exercises/pizza_project/pizzas/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
+
 def index(request):
+    """Render the index page for the pizza app."""
     return render(request, "pizzas/index.html")
+
+
+def pizzas(request):
+    """Show all pizzas."""
+    all_pizzas = Pizza.objects.order_by("name")
+    context = {
+        "pizzas": all_pizzas,
+    }
+    return render(request, "pizzas/pizzas.html", context)
+
+
+def pizza(request, pizza_id):
+    """Show a single pizza and all toppings."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    pizza_toppings = pizza.topping_set.order_by("-date_added")
+    context = {"pizza": pizza, "toppings": pizza_toppings}
+    return render(request, "pizzas/pizza.html", context)
