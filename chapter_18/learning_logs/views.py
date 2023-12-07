3a8ea54298bea3227baa3bf3a4b7b3db854a669a
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-07 20:42:23 +0100

    Create an option to add a topic

--- chapter_18/learning_logs/views.py
+++ chapter_18/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -21,3 +22,16 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valiid():
+            form.save()
+            return redirect("learning_logs:topics")
+
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
