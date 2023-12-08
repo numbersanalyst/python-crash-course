10a4ed2ca790515ecdf36217151ff663dd2c4498
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-08 16:57:22 +0100

    Create an option to edit an entry

--- chapter_18/learning_logs/views.py
+++ chapter_18/learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 from .forms import EntryForm, TopicForm
-from .models import Topic
+from .models import Entry, Topic
 
 
 def index(request):
@@ -54,3 +54,20 @@ def new_entry(request, topic_id):
 
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
