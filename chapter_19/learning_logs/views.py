e43d263e13f4bfeb37f92fccea07c76c04ef4dbf
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-18 12:12:53 +0100

    Add topic protection, and fix form validation

--- chapter_19/learning_logs/views.py
+++ chapter_19/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import redirect, render
 
 from .forms import EntryForm, TopicForm
@@ -13,17 +14,23 @@ def index(request):
 @login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
+
 
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
 
 @login_required
 def new_topic(request):
@@ -32,12 +39,15 @@ def new_topic(request):
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
-        if form.is_valiid():
-            form.save()
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("learning_logs:topics")
 
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
+
 
 @login_required
 def new_entry(request, topic_id):
@@ -57,11 +67,15 @@ def new_entry(request, topic_id):
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
 
+
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         form = EntryForm(instance=entry)
